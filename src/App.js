8cb364ff549d3dbc332a59620b87879a41f7ddbf
oscar50 <oscar@whaleandjaguar.co>
2023-08-14 20:19:24 -0500

    feat: first web

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import imagen from "../src/firma oscar .png"
 
 function App() {
+  let nombre = "Oscar";
+  let caja = <div>Nombre:</div>; // para enviar un html desde js
+  let nombres = ["Jab", "Pepito", "Jose"];
+  let nombres_json = [{nombre: "Angélica",
+                      edad: 16,
+                      nombre: "Oscar",
+                      edad: 44}]
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="font">
+      <h1>Oscar Riojas</h1>
+      <h1>{ nombres[1] }</h1>
+      <h2>Edad = { nombres_json.edad }</h2>
+      <h3>Edad json = { nombres_json[0].nombre}</h3>
+      <input value={ nombre }></input>
+      <div>{ caja }</div>
+      <div>{ nombre }</div>
+      {alert("Hola mundo")}
+      <br></br>
+      <img src={ imagen }></img>
     </div>
   );
 }
